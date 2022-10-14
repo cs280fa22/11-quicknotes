29538ec1e7d69fa723a06218758f79b6092d20f3
Ali Madooei <alimadooei@gmail.com>
2022-10-13 23:05:12 -0400

    Step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
+
 function App() {
-  return <>Hello, React!</>;
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const fakeNotes = [];
+    for (let index = 0; index < 10; index++) {
+      fakeNotes.push({
+        id: faker.datatype.uuid(),
+        title: faker.lorem.sentence(),
+        text: faker.lorem.paragraph(),
+      });
+    }
+    setNotes(fakeNotes);
+  }, []);
+
+  return (
+    <>
+      {
+        notes.map(note => (
+          <div>
+            <p>{note.title}</p>
+            <p>{note.text}</p>
+          </div>
+        ))
+      }
+    </>
+  );
 }
 
 export default App;
