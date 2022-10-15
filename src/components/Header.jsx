5156a646b522d362e48120a29b82efce5044bfbe
Ali Madooei <alimadooei@gmail.com>
2022-10-14 23:59:23 -0400

    Step 12

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,31 @@
+import { IconNote } from "@tabler/icons";
+import { Grid, Button } from "@mantine/core";
 import Search from "./Search";
 import PropTypes from "prop-types";
 
 function Header(props) {
   const { query, setQuery } = props;
 
+  const handleOnClick = () => {
+    // TODO implement me!
+  };
+
   return (
-    <>
-      <Search query={query} setQuery={setQuery} />
-    </>
+    <Grid>
+      <Grid.Col span={10}>
+        <Search query={query} setQuery={setQuery} />
+      </Grid.Col>
+      <Grid.Col span={2}>
+        <Button
+          onClick={handleOnClick}
+          fullWidth
+          variant="default"
+          leftIcon={<IconNote />}
+        >
+          Add Note
+        </Button>
+      </Grid.Col>
+    </Grid>
   );
 }
 
